7cb28a8c3f8d9146f65639cccfc836f9719daa5c
Tngai <Tngai@msn.com>
2017-10-10 03:44:25 -0700

    created article card component

--- components/card.js
+++ components/card.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, TouchableHighlight, Alert } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import Layout from '../constants/Layout';
-import { MonoText } from './StyledText';
+import { WebBrowser } from 'expo';
+import key from '../newsApi';
 
 export default class Card extends Component {
 	formatDate (date = '') {
@@ -9,43 +10,43 @@ export default class Card extends Component {
 		return dateObj.toDateString();
 	}
 
-	handlePress(e) {
-		return Alert.alert('e ', e);
+	handlePress() {
+		WebBrowser.openBrowserAsync('https://expo.io');
 	}
 
 	render() {
-		const { imageUri, author, date, title, description } = this.props;
+		const { imageUri, author, date, title, description } = this.props.item;
 		const dateTime = this.formatDate(date);
+		console.warn('key ', key);
 		return (
-			<TouchableHighlight onPress={this.handlePress}>
+			<TouchableOpacity onPress={this.handlePress}>
 				<View style={styles.cardContainer}>
-					<Image
-	        	source={{ uri: imageUri }}
-	        	style={styles.cardImage}
-	        	resizeMode="cover"
-	        />
-			    <Text style={styles.titleText}>{ title }</Text>
-			    <View style={styles.metaContainer}>
-			    	<Text style={styles.author}>{ author }</Text>
-			    	<Text style={styles.date}>{ dateTime }</Text>
-			    </View>
-			    <Text style={styles.description}>{ description }</Text>
+						<Image
+		        	source={{ uri: imageUri }}
+		        	style={styles.cardImage}
+		        	resizeMode="cover"
+		        />
+				    <Text style={styles.titleText}>{ title }</Text>
+				    <View style={styles.metaContainer}>
+				    	<Text style={styles.author}>{ author }</Text>
+				    	<Text style={styles.date}>{ dateTime }</Text>
+				    </View>
+				    <Text style={styles.description}>{ description }</Text>
 				</View>
-			</TouchableHighlight>
+			</TouchableOpacity>
 		);
 	}
 }
 const styles = StyleSheet.create({
 	cardContainer: {
-		height: 350,
-		borderWidth: 3,
-		borderColor: 'green',
-		paddingBottom: 10,
+		height: 'auto',
+		paddingBottom: 10
 	},
 	cardImage: {
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
+		height: 250,
 		backgroundColor: 'rgba(0, 0, 0, 0)',
 	},
 	titleText: {
@@ -53,23 +54,24 @@ const styles = StyleSheet.create({
 		marginTop: 10,
     fontSize: 18,
     color: '#0d0d0d',
+    marginLeft: 5
 	},
 	metaContainer: {
-		marginTop: 10,
+		margin: 5,
 		flexDirection:'row',
 	},
 	author: {
 		flex: 1,
-		fontSize: 10,
+		fontSize: 12,
 		justifyContent: 'flex-start',
 	},
 	date: {
 		flex: 1,
-		fontSize: 10,
+		fontSize: 12,
 		textAlign: 'right',
 	},
 	description: {
-		marginTop: 16,
+		padding: 10,
 		textAlign: 'justify'
 	}
 });
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import Card from '../components/card';
+import
 
 const data = [
   {
@@ -9,7 +10,16 @@ const data = [
     title: 'Honest Company may be raising a down round',
     author: 'Connie Loizos',
     date: '2017-10-05T22:46:12Z',
-    description: "The Honest Company, the five-year-old natural body and home care products company cofounded by the actress Jessica Alba, looks to be raising $75 million in..",
+    description: 'The Honest Company, the five-year-old natural body and home care products company cofounded by the actress Jessica Alba, looks to be raising $75 million in..',
+    articleUrl: 'https://techcrunch.com/2017/10/05/honest-company-may-be-raising-a-down-round/'
+  },
+  {
+    imageUri: 'https://tctechcrunch2011.files.wordpress.com/2015/04/shutterstock_245957161.jpg',
+    title: 'Nokia pulls out of OZO VR hardware, lays off 310, steps up in health and patents',
+    author: 'Connie Loizos',
+    date: '2017-10-10T07:18:44Z',
+    description: 'Nokia, the once-mighty phone maker that eventually retreated to a business based around networking equipment and targeted verticals like health and imaging,..',
+    articleUrl: 'https://techcrunch.com/2017/10/10/nokia-pulls-out-of-ozo-vr-hardware-lays-off-310-steps-up-in-health-and-patents/'
   }
 ];
 
@@ -18,11 +28,25 @@ export default class LinksScreen extends React.Component {
     title: 'Links',
   };
 
+  _renderItem = (item) => {
+    return (
+      <Card {...item} />
+    );
+  };
+
+  _keyExtractor = (item, index) => {
+    return `${index}-${item.date}`;
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Card {...data[0]} />
-      </View>
+        <View style={styles.container}>
+        <FlatList style={styles.scrollList}
+          data={data}
+          renderItem={this._renderItem}
+          keyExtractor={this._keyExtractor}
+        />
+        </View>
     );
   }
 }
@@ -30,8 +54,9 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderColor: 'blue',
+    backgroundColor: '#fff'
   },
+  scrollList: {
+    height: 800
+  }
 });
